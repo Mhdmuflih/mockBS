ad8836bd6de53c2909f9d7407e633a35e17c523a
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-02-27 17:21:22 +0530

    reduce the space

--- api-gateway/src/gateway.ts
+++ api-gateway/src/gateway.ts
@@ -20,7 +20,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(morgan("tiny"));
 
-
 app.use("/auth-service", proxy(process.env.Auth_Service as string || "http://localhost:1010"));
 app.use("/user-service", verifyJWT, proxy(process.env.User_Management_Service as string || "http://localhost:2020"));
 app.use("/booking-service", verifyJWT, proxy(process.env.Booking_Service as string || "http://localhost:3030"));

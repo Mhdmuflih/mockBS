c799255d45758caa3825b59f31d89700d7af68e6
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-02-28 16:31:23 +0530

    ok

--- api-gateway/src/gateway.ts
+++ api-gateway/src/gateway.ts
@@ -5,7 +5,6 @@ import proxy from "express-http-proxy";
 import dotenv from "dotenv";
 import { verifyJWT } from "./middleware/jwtVerification";
 
-
 dotenv.config();
 
 const app: Application = express();
@@ -24,7 +23,6 @@ app.use("/auth-service", proxy(process.env.Auth_Service as string || "http://loc
 app.use("/user-service", verifyJWT, proxy(process.env.User_Management_Service as string || "http://localhost:2020"));
 app.use("/booking-service", verifyJWT, proxy(process.env.Booking_Service as string || "http://localhost:3030"));
 app.use("/payment-service", verifyJWT, proxy(process.env.Payment_Service as string || "http://localhost:4040"));
-
 
 app.use("*", (req: Request, res: Response) => {
     res.status(404).json({ message: "Route not found" });

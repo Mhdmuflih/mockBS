7b438f14865be6d4e49b7a55857faa4944c8e099
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-03-16 17:25:22 +0530

    socket connection added in api gateway

--- api-gateway/src/gateway.ts
+++ api-gateway/src/gateway.ts
@@ -4,25 +4,35 @@ import morgan from "morgan";
 import proxy from "express-http-proxy";
 import dotenv from "dotenv";
 import { verifyJWT } from "./middleware/jwtVerification";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { createServer, Server } from "http";
 
 dotenv.config();
 
 const app: Application = express();
 
 const corsOptions = {
-    origin: process.env.FrontEnd, 
+    origin: process.env.FrontEnd,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true, 
+    credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
 app.use(morgan("tiny"));
 
+
 app.use("/auth-service", proxy(process.env.Auth_Service as string || "http://localhost:1010"));
 app.use("/user-service", verifyJWT, proxy(process.env.User_Management_Service as string || "http://localhost:2020"));
 app.use("/booking-service", verifyJWT, proxy(process.env.Booking_Service as string || "http://localhost:3030"));
 app.use("/payment-service", verifyJWT, proxy(process.env.Payment_Service as string || "http://localhost:4040"));
+
+app.use('/socket.io', createProxyMiddleware({
+    target:"http://localhost:3000",
+    ws: true,
+    changeOrigin: true,
+}));
+
 
 app.use("*", (req: Request, res: Response) => {
     res.status(404).json({ message: "Route not found" });

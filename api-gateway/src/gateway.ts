62c61411e16bb3fd81986e139b57cca5d3660cc2
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-03-28 21:52:06 +0530

    review service added in gateway

--- api-gateway/src/gateway.ts
+++ api-gateway/src/gateway.ts
@@ -21,11 +21,18 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(morgan("tiny"));
 
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+
+
 
 app.use("/auth-service", proxy(process.env.Auth_Service as string || "http://localhost:1010"));
 app.use("/user-service", verifyJWT, proxy(process.env.User_Management_Service as string || "http://localhost:2020"));
 app.use("/booking-service", verifyJWT, proxy(process.env.Booking_Service as string || "http://localhost:3030"));
 app.use("/payment-service", verifyJWT, proxy(process.env.Payment_Service as string || "http://localhost:4040"));
+app.use('/review-service', verifyJWT, proxy(process.env.Review_Service as string || "http://localhost:5050") )
 
 app.use('/socket.io', createProxyMiddleware({
     target:"http://localhost:3000",

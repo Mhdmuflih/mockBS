c679dc8421cd2be407751c34fa31c199de59a584
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-04-09 16:07:39 +0530

    logger and chat-service redirection added

--- api-gateway/src/gateway.ts
+++ api-gateway/src/gateway.ts
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import { verifyJWT } from "./middleware/jwtVerification";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { createServer, Server } from "http";
+import logger from "./middleware/logger";
 
 dotenv.config();
 
@@ -21,8 +22,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(morgan("tiny"));
 
+// Custom logging middleware
 app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
+    logger.info(`Incoming request: ${req.method} ${req.url}`);
     next();
 });
 
@@ -32,7 +34,8 @@ app.use("/auth-service", proxy(process.env.Auth_Service as string || "http://loc
 app.use("/user-service", verifyJWT, proxy(process.env.User_Management_Service as string || "http://localhost:2020"));
 app.use("/booking-service", verifyJWT, proxy(process.env.Booking_Service as string || "http://localhost:3030"));
 app.use("/payment-service", verifyJWT, proxy(process.env.Payment_Service as string || "http://localhost:4040"));
-app.use('/review-service', verifyJWT, proxy(process.env.Review_Service as string || "http://localhost:5050") )
+app.use('/review-service', verifyJWT, proxy(process.env.Review_Service as string || "http://localhost:5050") );
+app.use('/chat-service', verifyJWT, proxy(process.env.Chat_Service as string || "http://localhost:6006"));
 
 app.use('/socket.io', createProxyMiddleware({
     target:"http://localhost:3000",

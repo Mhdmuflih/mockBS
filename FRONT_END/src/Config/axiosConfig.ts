8fd86145d0f4a0a2ed20cf8e44c4d8722fef777c
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-04-18 12:14:08 +0530

    done

--- FRONT_END/src/Config/axiosConfig.ts
+++ FRONT_END/src/Config/axiosConfig.ts
@@ -4,6 +4,9 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
+console.log("Resolved Base URL:", baseURL); // Should show your backend URL
+
+
 const UnProtectedAPI = axios.create({
     baseURL: `${baseURL}/auth-service`
 });

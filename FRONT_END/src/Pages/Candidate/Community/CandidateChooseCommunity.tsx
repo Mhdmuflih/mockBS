bc90695a294c7600726961fdabfbbde3338c508e
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-04-28 18:00:54 +0530

    docker compose file added and yaml file added

--- FRONT_END/src/Pages/Candidate/Community/CandidateChooseCommunity.tsx
+++ FRONT_END/src/Pages/Candidate/Community/CandidateChooseCommunity.tsx
@@ -11,7 +11,7 @@ import { ICandidatePremiumApiResponse, ICandidateProfileApiResponse } from "../.
 import { ICandidate, IGroup, IGroupMessage } from "../../../Interface/candidateInterfaces/interface";
 
 // const socket = io('ws://localhost:6060', { transports: ['websocket'] }); // Use correct server URL
-const socket = io('wss://api.muflih.online/ws/chat/', { transports: ['websocket'] }); // Use correct server URL
+const socket = io('wss://api.muflih.online:6060', { transports: ['websocket'] }); // Use correct server URL
 
 
 const CandidateChooseCommunity = () => {

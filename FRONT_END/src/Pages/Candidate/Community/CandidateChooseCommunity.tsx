51615c14fdcaf1fbee85ebe6ff2cc7ee0bb59242
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-04-29 17:29:24 +0530

    socket connection change

--- FRONT_END/src/Pages/Candidate/Community/CandidateChooseCommunity.tsx
+++ FRONT_END/src/Pages/Candidate/Community/CandidateChooseCommunity.tsx
@@ -11,15 +11,9 @@ import { ICandidatePremiumApiResponse, ICandidateProfileApiResponse } from "../.
 import { ICandidate, IGroup, IGroupMessage } from "../../../Interface/candidateInterfaces/interface";
 
 // const socket = io('ws://localhost:6060', { transports: ['websocket'] }); // Use correct server URL
-// const socket = io('ws://api.muflih.online:6060', { transports: ['websocket'] });
+const socket = io('ws://api.muflih.online:6060', { transports: ['websocket'] });
 
 
-const socket = io("wss://api.muflih.online:6060/chat", {
-    transports: ['websocket'],
-    withCredentials: true,
-  });
-  
-  
 const CandidateChooseCommunity = () => {
 
     const [candidateData, setCandidateData] = useState<ICandidate | null>(null)

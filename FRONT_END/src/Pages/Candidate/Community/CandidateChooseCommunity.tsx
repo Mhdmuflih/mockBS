3bed2c68352bf5996860c7d05c2a97a04637919b
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-04-29 10:43:34 +0530

    socket connection change

--- FRONT_END/src/Pages/Candidate/Community/CandidateChooseCommunity.tsx
+++ FRONT_END/src/Pages/Candidate/Community/CandidateChooseCommunity.tsx
@@ -11,8 +11,11 @@ import { ICandidatePremiumApiResponse, ICandidateProfileApiResponse } from "../.
 import { ICandidate, IGroup, IGroupMessage } from "../../../Interface/candidateInterfaces/interface";
 
 // const socket = io('ws://localhost:6060', { transports: ['websocket'] }); // Use correct server URL
-const socket = io('wss://ws.muflih.online', { transports: ['websocket'] });
-
+const socket = io('wss://api.muflih.online/chat-gateway', {
+    path: "/socket.io", // default
+    transports: ['websocket'], // force WebSocket (optional but good)
+    withCredentials: true,
+  });
 
 const CandidateChooseCommunity = () => {
 
